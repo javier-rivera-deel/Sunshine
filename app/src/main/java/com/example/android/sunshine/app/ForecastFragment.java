8691470bb7cbbd1573611ef568b9dcf20884d277
Javier Rivera <arq.javierriver@gmail.com>
2016-03-13 11:16:38 -0430

    Add refresh button
    Add some methods on ForecastFragment class:
    
    onCreate();
    onCreateOptionsMenu(); to inflate the menu item
    
    Now the app has a refresh button on the menu. It doesn't do anything,
    yet.

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -29,9 +32,32 @@ public class ForecastFragment extends Fragment {
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        //Add this line in order for this fragment to handle menu events
+        setHasOptionsMenu(true);
+    }
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
+        inflater.inflate(R.menu.forecast_fragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id == R.id.action_refresh){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+
+
         ArrayAdapter<String> mForecastAdapter;
         String[] forecastArray = {
                 "Today - Sunny - 88/50",
@@ -41,6 +67,8 @@ public class ForecastFragment extends Fragment {
                 "Fri - Cloudy - 88/50",
                 "Sat - Rain - 88/50",
         };
+
+
 
         List<String> weekForecast = new ArrayList<String>(
                 Arrays.asList(forecastArray));
