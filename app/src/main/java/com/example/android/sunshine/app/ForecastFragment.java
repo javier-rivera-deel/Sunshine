db3664e34bc68ead504c9eb9fc52a9d8593420ee
Javier Rivera <arq.javierriver@gmail.com>
2016-03-17 13:19:54 -0430

    Let's toast!
    Added functionality that permits a click event handler on item list,
    displaying a toast message with the forecast of the item clicked.

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -1,5 +1,6 @@
 package com.example.android.sunshine.app;
 
+import android.content.Context;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -12,8 +13,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -94,6 +97,18 @@ public class ForecastFragment extends Fragment {
 
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Context context = getActivity();
+                CharSequence forecast = mForecastAdapter.getItem(i).toString();
+                CharSequence text = "Hello toast!";
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, forecast, duration);
+                toast.show();
+            }
+        });
 
         return rootView;
     }
